f83060687f7705591cbd2af378891b4db7760f9c
ayano-opendna <ayano@open-dna.jp>
2014-04-22 12:07:33 +0900

    ???

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScorerankingApi::Application.config.secret_token = 'eaa7923fa8547dd39ee5d0d41c404ca4eb700a4da3641ab66515d970aeae67098bcef34575b6c3554274f249e3c9853ed183b26cbc12c5f62d54b206c6809396'
